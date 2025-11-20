55c359de9b9fdbd45f25415585d5ce45fa5de8b1
Gabriellae11 <gabriella8803@gmail.com>
2025-11-20 18:12:52 +0700

    fix: mainactivity

--- app/src/main/java/com/example/doirag/MainActivity.java
+++ app/src/main/java/com/example/doirag/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.doirag;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
-
 import com.example.doirag.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,15 +18,15 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-    }
+        // 1. Find the NavHostFragment safely
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_main);
 
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+
+            // 2. Connect Bottom Navigation to the Controller
+            NavigationUI.setupWithNavController(binding.navView, navController);
+        }
+    }
 }
